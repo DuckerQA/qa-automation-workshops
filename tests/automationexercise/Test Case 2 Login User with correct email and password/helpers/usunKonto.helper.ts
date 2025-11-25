8886c9a5dde43042c90736a8956912ef7e5fd4b6
mateuszQA <mateusz.czerewko@wearecogworks.com>
2025-11-25 15:20:06 +0100

    chore: configure environment settings

--- tests/automationexercise/Test Case 2 Login User with correct email and password/helpers/usunKonto.helper.ts
+++
@@ -1,9 +0,0 @@
-import { Page, expect} from "@playwright/test";
-
-export const usunKontoHelper = {
-    async usunKonto(page: Page){
-        await page.getByRole('link', { name: ' Delete Account' }).click();
-        expect(await page.getByText('Account Deleted!')).toBeVisible();
-        await page.getByRole('link', { name: 'Continue' }).click();
-    }
-}
