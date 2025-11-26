e99b8e3f8be971a9440e72fde22d316720a38914
mateuszQA <mateusz.czerewko@wearecogworks.com>
2025-11-26 11:38:04 +0100

    feat(pom): add RegisterPage

---
+++ src/pages/register.page.ts
@@ -0,0 +1,95 @@
+import { Locator, Page } from '@playwright/test';
+
+import { UserInitModel, UserModel } from '../models/user.model';
+import { BasePage } from './base.page';
+
+export class RegisterPage extends BasePage {
+  readonly url = '/register';
+
+  // --- SIGNUP FORM ---
+  readonly signUpLoginButton = this.page.getByRole('link', {
+    name: 'Signup / Login',
+  });
+  readonly headerNewUserSignup = this.page.getByRole('heading', {
+    name: 'New User Signup!',
+  });
+  readonly signUpNameInput = this.page.locator('[data-qa="signup-name"]');
+  readonly signUpEmailInput = this.page.locator('[data-qa="signup-email"]');
+  readonly signUpButton = this.page.getByRole('button', { name: 'Signup' });
+
+  // --- ACCOUNT INFO SECTION ---
+
+  readonly genderMaleOption = this.page.locator('#uniform-id_gender1');
+  readonly passwordInput = this.page.locator('#password');
+  readonly birthDaySelect = this.page.locator('[data-qa="days"]');
+  readonly birthMonthSelect = this.page.locator('[data-qa="months"]');
+  readonly birthYearSelect = this.page.locator('[data-qa="years"]');
+  readonly newsletterCheckbox = this.page.locator('#newsletter');
+  readonly offersCheckbox = this.page.locator('#optin');
+
+  // --- ADDRESS INFO ---
+  readonly firstNameInput = this.page.locator('#first_name');
+  readonly lastNameInput = this.page.locator('#last_name');
+  readonly companyInput = this.page.locator('#company');
+  readonly address1Input = this.page.locator('#address1');
+  readonly address2Input = this.page.locator('#address2');
+  readonly countrySelect = this.page.locator('#country');
+  readonly stateInput = this.page.locator('#state');
+  readonly cityInput = this.page.locator('#city');
+  readonly zipcodeInput = this.page.locator('#zipcode');
+  readonly mobileNumberInput = this.page.locator('#mobile_number');
+
+  // --- CREATE ACCOUNT Confirmation ---
+
+  readonly createAccountButton = this.page.locator(
+    '[data-qa="create-account"]',
+  );
+  readonly continueButton = this.page.locator('[data-qa="continue-button"]');
+  readonly accountCreatedHeader = this.page.getByRole('heading', {
+    name: 'Account Created!',
+  });
+
+  // --- DELETE ACCOUNT ---
+  readonly deleteAccountButton = this.page.getByRole('link', {
+    name: 'Delete Account',
+  });
+  readonly accountDeletedHeader = this.page.getByText('Account Deleted!');
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async initAccountCreation(userInit: UserInitModel): Promise<void> {
+    await this.signUpNameInput.fill(userInit.name);
+    await this.signUpEmailInput.fill(userInit.email);
+    await this.signUpButton.click();
+  }
+
+  async completeRegistrationDetails(user: UserModel): Promise<void> {
+    //Account Info
+    await this.genderMaleOption.click();
+    await this.passwordInput.fill(user.password);
+    await this.birthDaySelect.selectOption(user.day);
+    await this.birthMonthSelect.selectOption(user.month);
+    await this.birthYearSelect.selectOption(user.year);
+    await this.newsletterCheckbox.check();
+    await this.offersCheckbox.check();
+
+    //Address Info
+    await this.firstNameInput.fill(user.firstName);
+    await this.lastNameInput.fill(user.lastName);
+    await this.companyInput.fill(user.company);
+    await this.address1Input.fill(user.address1);
+    await this.address2Input.fill(user.address2);
+    await this.countrySelect.selectOption(user.country);
+    await this.stateInput.fill(user.state);
+    await this.cityInput.fill(user.city);
+    await this.zipcodeInput.fill(user.zipcode);
+    await this.mobileNumberInput.fill(user.mobileNumber);
+    await this.createAccountButton.click();
+  }
+
+  loggedInAs(userName: string): Locator {
+    return this.page.getByText(`Logged in as ${userName}`);
+  }
+}
--- tests/smoke/register.spec.ts
+++ tests/smoke/register.spec.ts
@@ -1,59 +1,36 @@
 import { expect, test } from '@playwright/test';
 
-test('register user with correct data @KAC_01_01', async ({ page }) => {
-  await page.goto('https://www.automationexercise.com/');
-  await page.addLocatorHandler(
-    page.locator('.fc-dialog-headline'),
-    async () => {
-      await page.getByRole('button', { name: 'Consent' }).click();
-    },
-  );
+import {
+  getRandomInitData,
+  prepareRandomUserData,
+} from '../../src/factories/user.factory';
+import { UserInitModel, UserModel } from '../../src/models/user.model';
+import { RegisterPage } from '../../src/pages/register.page';
 
-  await page.getByRole('link', { name: ' Signup / Login' }).click();
-  await expect(
-    page.getByRole('heading', { name: 'New User Signup!' }),
-  ).toBeVisible();
-  await page.getByRole('textbox', { name: 'Name' }).fill('Potato');
-  await page
-    .locator('form')
-    .filter({ hasText: 'Signup' })
-    .getByPlaceholder('Email Address')
-    .fill('test@test123SADASDas.com');
-  await page.getByRole('button', { name: 'Signup' }).click();
-  await page.getByRole('radio', { name: 'Mr.' }).check();
-  await page.getByRole('textbox', { name: 'Password *' }).fill('test1');
-  await page.locator('#days').selectOption('1');
-  await page.locator('#months').selectOption('2');
-  await page.locator('#years').selectOption('2003');
-  await page
-    .getByRole('checkbox', { name: 'Sign up for our newsletter!' })
-    .check();
-  await page
-    .getByRole('checkbox', { name: 'Receive special offers from' })
-    .check();
-  await page.getByRole('textbox', { name: 'First name *' }).fill('Name');
-  await page.getByRole('textbox', { name: 'Last name *' }).fill('LastName');
-  await page
-    .getByRole('textbox', { name: 'Company', exact: true })
-    .fill('Company');
-  await page
-    .getByRole('textbox', { name: 'Address * (Street address, P.' })
-    .fill('Adress, 12 Orla');
-  await page.getByRole('textbox', { name: 'Address 2' }).fill('Adres 2 ');
-  await page.getByLabel('Country *').selectOption('Australia');
-  await page.getByRole('textbox', { name: 'State *' }).fill('London');
-  await page.getByRole('textbox', { name: 'State *' }).press('ControlOrMeta+a');
-  await page.getByRole('textbox', { name: 'State *' }).fill('State');
-  await page.getByRole('textbox', { name: 'City * Zipcode *' }).fill('London');
-  await page.locator('#zipcode').fill('12444');
-  await page
-    .getByRole('textbox', { name: 'Mobile Number *' })
-    .fill('700300500');
-  await page.getByRole('button', { name: 'Create Account' }).click();
-  await expect(page.getByText('Account Created!')).toBeVisible();
-  await page.getByRole('link', { name: 'Continue' }).click();
-  await expect(page.getByText('Logged in as Potato')).toBeVisible();
-  await page.getByRole('link', { name: ' Delete Account' }).click();
-  await expect(page.getByText('Account Deleted!')).toBeVisible();
-  await page.getByRole('link', { name: 'Continue' }).click();
+test('register user with correct data @AUT_01-01', async ({ page }) => {
+  //Arrange
+  const registerPage = new RegisterPage(page);
+  const initData: UserInitModel = getRandomInitData();
+  const userData: UserModel = prepareRandomUserData();
+
+  //Act
+  await registerPage.goto();
+  await registerPage.getTitle();
+  await expect(page).toHaveURL(/.*automationexercise.com/);
+  await registerPage.cookieHandler();
+
+  await registerPage.signUpLoginButton.click();
+  await expect(registerPage.headerNewUserSignup).toBeVisible();
+
+  await registerPage.initAccountCreation(initData);
+  await registerPage.completeRegistrationDetails(userData);
+
+  await expect(registerPage.accountCreatedHeader).toBeVisible();
+  await registerPage.continueButton.click();
+
+  //Assert
+  await expect(registerPage.loggedInAs(initData.name)).toBeVisible();
+  await registerPage.deleteAccountButton.click();
+  await expect(registerPage.accountDeletedHeader).toBeVisible();
+  await registerPage.continueButton.click();
 });
