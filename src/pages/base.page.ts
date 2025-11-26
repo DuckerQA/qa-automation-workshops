f1dd6e90197f998a024c32f1d0d2107d63efdd8f
mateuszQA <mateusz.czerewko@wearecogworks.com>
2025-11-26 11:38:12 +0100

    feat(pom): add BasePage

---
+++ src/pages/base.page.ts
@@ -0,0 +1,28 @@
+import { Page } from '@playwright/test';
+
+export class BasePage {
+  url = '';
+  constructor(protected page: Page) {}
+
+  async goto(): Promise<void> {
+    await this.page.goto(this.url);
+  }
+
+  async getTitle(): Promise<string> {
+    await this.page.waitForLoadState('load');
+    return this.page.title();
+  }
+
+  async waitForPageToLoad(): Promise<void> {
+    await this.page.waitForURL(this.url);
+  }
+
+  async cookieHandler(): Promise<void> {
+    await this.page.addLocatorHandler(
+      this.page.locator('.fc-dialog-headline'),
+      async () => {
+        await this.page.getByRole('button', { name: 'Consent' }).click();
+      },
+    );
+  }
+}
