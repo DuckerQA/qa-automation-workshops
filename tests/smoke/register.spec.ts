205e81ae1606ba2e0ff936bac6eb931a0340858c
Kacper <pikapikaczek@gmail.com>
2025-12-07 12:20:38 +0100

    fix: update URL expectation in registration test to use root path

--- tests/smoke/register.spec.ts
+++ tests/smoke/register.spec.ts
@@ -27,7 +27,7 @@ test('Case 1: Register User @AUT_01', async ({ page }) => {
   await test.step('Open page and accept cookies', async () => {
     await registerPage.open();
     await registerPage.getTitle();
-    await expect(page).toHaveURL(process.env.BASE_URL!);
+    await expect(page).toHaveURL('/');
     await registerPage.cookieHandler();
   });
 
